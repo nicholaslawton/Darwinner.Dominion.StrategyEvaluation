3bdd17833651d5e3b367135f286ded62db2ee3ab
Unknown <nick.a.lawton@gmail.com>
2018-11-22 00:26:58 +1100

    Engine basics

--- src/Engine.hs
+++ src/Engine.hs
@@ -1,9 +1,22 @@
 module Engine (runUntil) where
 
 import Game
+import Event
 
 import Control.Monad
 import Control.Monad.Trans.State
 
-runUntil :: (GameState -> Bool) -> State Game ()
-runUntil _ = void get
+runUntil :: (Game -> Bool) -> State Game ()
+runUntil predicate = do
+  game <- get
+  unless (predicate game) $ do
+    let event = nextEvent game
+    let newGame = update event game
+    put newGame
+    runUntil predicate
+
+nextEvent :: Game -> Event
+nextEvent _ = Noop
+
+update :: Event -> Game -> Game
+update event game = game { history = event : history game }
--- test/GamePreparationTests.hs
+++ test/GamePreparationTests.hs
@@ -7,9 +7,10 @@ import Engine
 
 import Data.List
 import Data.Maybe
+import Control.Applicative
 import Control.Monad.Trans.State
 
-import ArbitraryInstances
+import ArbitraryInstances()
 import Test.Hspec
 import Test.QuickCheck
 
@@ -17,14 +18,16 @@ gamePreparationTests :: SpecWith ()
 gamePreparationTests = describe "game preparation" $ do
 
   it "adds all players" $ property $ \seed players ->
-    (playersAdded . Game.history . execUntil (== Prepared)) (Game.new seed)
-      == sort (playerId <$> players)
+    (playerIds . mapMaybe playerAdded . Game.history . execUntil prepared) (Game.new seed) == playerIds players
 
-execUntil :: (GameState -> Bool) -> Game -> Game
+execUntil :: (Game -> Bool) -> Game -> Game
 execUntil predicate = execState (Engine.runUntil predicate)
 
-playersAdded :: [Event] -> [PlayerId]
-playersAdded = sort . fmap playerId . mapMaybe playerAdded
+prepared :: Game -> Bool
+prepared = liftA2 (||) ((== Prepared) . Game.state) ((>10) . length . Game.history)
 
 playerAdded :: Event -> Maybe Player
 playerAdded Noop = Nothing
+
+playerIds :: [Player] -> [PlayerId]
+playerIds = sort . fmap playerId
