5c126b3f706d58cb35b684108cb843e7f2d478d9
Unknown <nick.a.lawton@gmail.com>
2018-12-18 19:57:01 +1100

    Verification that players ready begins preparing supply

--- test/UpdateTests.hs
+++ test/UpdateTests.hs
@@ -14,3 +14,11 @@ updateTests = describe "update" $ do
   describe "add player to new game" $
     it "adds player" $ property $ \ps player ->
       length (players (update (AddPlayer player) (New ps))) == length ps + 1
+
+  describe "players ready" $
+    it "begins preparing supply" $ property $
+      isPreparingSupply . update PlayersReady . New
+
+isPreparingSupply :: GameState -> Bool
+isPreparingSupply (PreparingSupply _ _) = True
+isPreparingSupply _ = False
