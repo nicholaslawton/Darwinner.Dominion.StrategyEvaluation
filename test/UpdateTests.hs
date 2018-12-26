943d0566fb9726b9329f2a0ce04e445403cad41b
Unknown <nick.a.lawton@gmail.com>
2018-12-26 11:51:28 +1100

    Use (===) in QuickCheck properties instead of (==)

--- test/UpdateTests.hs
+++ test/UpdateTests.hs
@@ -20,7 +20,7 @@ updateTests = describe "update" $ do
 
   describe "add player to new game" $
     it "adds player" $ property $ \ps player ->
-      length (players (update (AddPlayer player) (New ps))) == length ps + 1
+      length (players (update (AddPlayer player) (New ps))) === length ps + 1
 
   describe "players ready" $
     it "begins preparing supply" $ property $
@@ -28,7 +28,7 @@ updateTests = describe "update" $ do
 
   describe "place card in supply" $
     it "adds card" $ property $ \ps cards card ->
-      length (supply (update (PlaceCardInSupply card) (PreparingSupply ps cards))) == length cards + 1
+      length (supply (update (PlaceCardInSupply card) (PreparingSupply ps cards))) === length cards + 1
 
   describe "supply ready" $
     it "begins preparing decks" $ property $ \ps cards ->
@@ -37,7 +37,7 @@ updateTests = describe "update" $ do
   describe "add card to deck of player" $
     it "adds card to deck of player" $ property $ \(SelectedPlayer ps pid) cards card ->
       fmap (length . deck) (findPlayer pid (players (update (AddCardToDeck pid card) (PreparingDecks ps cards))))
-        == fmap ((+1) . length . deck) (findPlayer pid ps)
+        === fmap ((+1) . length . deck) (findPlayer pid ps)
 
   describe "decks ready" $
     it "begins drawing initial hands" $ property $ \ps cards ->
@@ -46,11 +46,11 @@ updateTests = describe "update" $ do
   describe "draw card" $ do
     it "adds card to hand" $ property $ \(CardInDeck ps pid card) cards ->
       fmap (length . hand) (findPlayer pid (players (update (DrawCard pid card) (DrawingInitialHands ps cards))))
-        == fmap ((+1) . length . hand) (findPlayer pid ps)
+        === fmap ((+1) . length . hand) (findPlayer pid ps)
 
     it "does not alter dominion of player" $ property $ \(CardInDeck ps pid card) cards ->
       fmap dominion (findPlayer pid (players (update (DrawCard pid card) (DrawingInitialHands ps cards))))
-        == fmap dominion (findPlayer pid ps)
+        === fmap dominion (findPlayer pid ps)
 
 findPlayer :: CandidateId -> [Player] -> Maybe Player
 findPlayer pid = find ((==) pid . playerId)
