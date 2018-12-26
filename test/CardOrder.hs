fa716fcab8dd7d65ed3df64cb6556a2cff3f3bca
Unknown <nick.a.lawton@gmail.com>
2018-12-26 11:26:30 +1100

    Explanatory comment for arbitrary card order

--- test/CardOrder.hs
+++ test/CardOrder.hs
@@ -2,6 +2,9 @@ module CardOrder (arbitraryCardOrder) where
 
 import Card
 
+-- There is no natural ordering of cards. However, it is necessary to be able to order them for the
+-- purposes of tests which verify the total set, regardless of order. The actual order is not
+-- important, as long as it is consistent.
 arbitraryCardOrder :: Card -> Int
 arbitraryCardOrder Province = 1
 arbitraryCardOrder Duchy = 2
