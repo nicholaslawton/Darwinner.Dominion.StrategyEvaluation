974dca3a67bb81e70c05ea13d4b3f5726297faba
Unknown <nick.a.lawton@gmail.com>
2018-12-27 15:07:09 +1100

    Converted to state pattern

--- src/Engine.hs
+++ src/Engine.hs
@@ -27,44 +27,57 @@ runUntil predicate = do
   game <- lift get
   unless (predicate game) $ do
     parameters <- ask
-    let (cmd, newGen) = nextCommand parameters (Game.state game) (gen game)
-    lift . put $ apply cmd (game { gen = newGen })
+    cmd <- lift $ nextCommand parameters
+    lift . put $ apply cmd game
     runUntil predicate
 
 apply :: Command -> Game -> Game
 apply command = recordCommand command . Game.mapState (update command)
 
-nextCommand :: EvaluationParameters -> GameState -> StdGen -> (Command, StdGen)
-nextCommand (EvaluationParameters candidates) (New ps) g =
-  flip (,) g $ fromMaybe MarkPlayersReady $ AddPlayer <$> nextPlayer
-    where
-      nextPlayer = listToMaybe $ filter (not . inGame) $ candidateId <$> candidates
-      inGame cid = elem cid (playerId <$> ps)
-nextCommand (EvaluationParameters candidates) (PreparingSupply _ cards) g
-  | countElem Copper cards < 60 - length candidates * 7 = (PlaceCardInSupply Copper, g)
-  | countElem Silver cards < 40 = (PlaceCardInSupply Silver, g)
-  | countElem Gold cards < 30 = (PlaceCardInSupply Gold, g)
-  | countElem Estate cards < numVictoryCards = (PlaceCardInSupply Estate, g)
-  | countElem Duchy cards < numVictoryCards = (PlaceCardInSupply Duchy, g)
-  | countElem Province cards < numVictoryCards = (PlaceCardInSupply Province, g)
-  | otherwise = (MarkSupplyPrepared, g)
-    where
-      numVictoryCards = if length candidates == 2 then 8 else 12
-nextCommand _ (PreparingDecks ps _) g =
-  flip (,) g $ fromMaybe MarkDecksPrepared $ uncurry AddCardToDeck <$> playerNeedingCard ps
-    where
-      playerNeedingCard :: [Player] -> Maybe (CandidateId, Card)
-      playerNeedingCard = liftA2 (<|>) (playerNeeding Copper 7) (playerNeeding Estate 3)
-      playerNeeding :: Card -> Int -> [Player] -> Maybe (CandidateId, Card)
-      playerNeeding card target =
-        fmap (flip (,) card . playerId) . listToMaybe . filter ((< target) . countElem card . deck)
-nextCommand _ (DrawingInitialHands ps _) g =
-  fromMaybe (Noop, g) $ flip drawCard g <$> playerWithIncompleteHand ps
-    where
-      playerWithIncompleteHand :: [Player] -> Maybe Player
-      playerWithIncompleteHand = listToMaybe . filter ((< 5) . length . hand)
-      drawCard :: Player -> StdGen -> (Command, StdGen)
-      drawCard p = first (DrawCard (playerId p)) . randomElement (deck p)
-      randomElement :: [a] -> StdGen -> (a, StdGen)
-      randomElement xs = first (xs !!) . randomR (0, length xs)
-nextCommand _ Prepared g = (Noop, g)
+nextCommand :: EvaluationParameters -> State Game Command
+nextCommand (EvaluationParameters candidates) = do
+  game <- get
+  case Game.state game of
+
+    New ps ->
+      return $ fromMaybe MarkPlayersReady $ AddPlayer <$> nextPlayer
+        where
+          nextPlayer = listToMaybe $ filter (not . inGame) $ candidateId <$> candidates
+          inGame cid = elem cid (playerId <$> ps)
+
+    PreparingSupply _ cards
+      | countElem Copper cards < 60 - length candidates * 7 -> return $ PlaceCardInSupply Copper
+      | countElem Silver cards < 40 -> return $ PlaceCardInSupply Silver
+      | countElem Gold cards < 30 -> return $ PlaceCardInSupply Gold
+      | countElem Estate cards < numVictoryCards -> return $ PlaceCardInSupply Estate
+      | countElem Duchy cards < numVictoryCards -> return $ PlaceCardInSupply Duchy
+      | countElem Province cards < numVictoryCards -> return $ PlaceCardInSupply Province
+      | otherwise -> return $ MarkSupplyPrepared
+        where
+          numVictoryCards = if length candidates == 2 then 8 else 12
+
+    PreparingDecks ps _ ->
+      return $ fromMaybe MarkDecksPrepared $ uncurry AddCardToDeck <$> playerNeedingCard ps
+        where
+          playerNeedingCard :: [Player] -> Maybe (CandidateId, Card)
+          playerNeedingCard = liftA2 (<|>) (playerNeeding Copper 7) (playerNeeding Estate 3)
+          playerNeeding :: Card -> Int -> [Player] -> Maybe (CandidateId, Card)
+          playerNeeding card target =
+            fmap (flip (,) card . playerId) . listToMaybe . filter ((< target) . countElem card . deck)
+
+    DrawingInitialHands ps _ ->
+      fromMaybe (return Noop) $ drawCard <$> playerWithIncompleteHand ps
+        where
+          playerWithIncompleteHand :: [Player] -> Maybe Player
+          playerWithIncompleteHand = listToMaybe . filter ((< 5) . length . hand)
+          drawCard :: Player -> State Game Command
+          drawCard p = DrawCard (playerId p) <$> randomElement (deck p)
+
+    Prepared -> return Noop
+
+randomElement :: [a] -> State Game a
+randomElement xs = do
+  game <- get
+  let (x, newGen) = first (xs !!) . randomR (0, length xs) . Game.gen $ game
+  put $ game { gen = newGen }
+  return x
