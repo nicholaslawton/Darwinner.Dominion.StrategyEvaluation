e9b76dec3068c97c6d47364218563dae6ef16527
Unknown <nick.a.lawton@gmail.com>
2018-12-27 17:40:11 +1100

    Simplified arbitrary card ordering for verification

--- test/CardOrder.hs
+++ test/CardOrder.hs
@@ -5,10 +5,5 @@ import Card
 -- There is no natural ordering of cards. However, it is necessary to be able to order them for the
 -- purposes of tests which verify the total set, regardless of order. The actual order is not
 -- important, as long as it is consistent.
-arbitraryCardOrder :: Card -> Int
-arbitraryCardOrder Province = 1
-arbitraryCardOrder Duchy = 2
-arbitraryCardOrder Estate = 3
-arbitraryCardOrder Gold = 4
-arbitraryCardOrder Silver = 5
-arbitraryCardOrder Copper = 6
+arbitraryCardOrder :: Card -> String
+arbitraryCardOrder = show
