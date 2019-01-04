118fd2354d75427f40b32e29990b0bfca22af7b5
Unknown <nick.a.lawton@gmail.com>
2019-01-04 21:48:26 +1100

    Ignore order when comparing cards in play

--- test/UpdateTests.hs
+++ test/UpdateTests.hs
@@ -151,4 +151,4 @@ dominion :: Player -> [Card]
 dominion p = sortOn arbitraryCardOrder $ concatMap ($ p) [Player.deck, Player.hand, Player.discard]
 
 cardsInPlay :: GameState -> [Card]
-cardsInPlay gameState = concatMap dominion (players gameState) ++ supply gameState
+cardsInPlay gameState = sortOn arbitraryCardOrder $ concatMap dominion (players gameState) ++ supply gameState
