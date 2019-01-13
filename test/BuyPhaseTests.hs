44eaea9ba0a32e6b63c6d1b7016f1ac8ce7f8310
Unknown <nick.a.lawton@gmail.com>
2019-01-13 21:41:15 +1100

    Refactored buy phase game production

--- test/BuyPhaseTests.hs
+++ test/BuyPhaseTests.hs
@@ -5,6 +5,8 @@ import Game
 import GameState
 import Engine
 import EvaluationParameters
+import Player
+import Card
 
 import Control.Applicative
 import Control.Monad.Trans.Reader
@@ -17,14 +19,17 @@ import Test.QuickCheck
 buyPhaseTests :: SpecWith ()
 buyPhaseTests = describe "buy phase" $ do
 
-  it "gains a card" $ property $ \seed (Positive buys) (NonEmpty ps) (NonEmpty cards) ->
-    any gainCard $ history $ performBuyPhase 10 (EvaluationParameters []) (Game.mapState (const (BuyPhase (BuyAllowance buys) ps cards)) (Game.new seed))
+  it "gains a card" $ property $ \params (Positive buys) (NonEmpty ps) (NonEmpty cards) ->
+    any gainCard . history . performBuyPhase 10 params . gameInBuyPhase buys ps cards
 
-  it "completes" $ property $ \seed params (Positive buys) (NonEmpty ps) cards ->
-    (===) BuyPhaseComplete $ last $ history $ performBuyPhase (min buys (length cards) + 10) params (Game.mapState (const (BuyPhase (BuyAllowance buys) ps cards)) (Game.new seed))
+  it "completes" $ property $ \params (Positive buys) (NonEmpty ps) (NonEmpty cards) ->
+    (===) BuyPhaseComplete . last . history . performBuyPhase (min buys (length cards) + 10) params . gameInBuyPhase buys ps cards
+
+gameInBuyPhase :: Int -> [Player] -> [Card] -> Int -> Game
+gameInBuyPhase buys ps cards = Game.mapState (const (BuyPhase (BuyAllowance buys) ps cards)) . Game.new
 
 performBuyPhase :: Int -> EvaluationParameters -> Game -> Game
-performBuyPhase limit = execUntil (buyPhaseOver limit)
+performBuyPhase = execUntil . buyPhaseOver
 
 execUntil :: (Game -> Bool) -> EvaluationParameters -> Game -> Game
 execUntil predicate parameters = execState $ runReaderT (Engine.runUntil predicate) parameters
