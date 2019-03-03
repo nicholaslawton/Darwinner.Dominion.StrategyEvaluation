954562f02cb325337a10566a2e4864bcd76cc99b
Unknown <nick.a.lawton@gmail.com>
2019-03-03 19:57:16 +1100

    Implemented turnsCompleted

--- src/Turn.hs
+++ src/Turn.hs
@@ -1,10 +1,14 @@
 module Turn
   ( Turn(..)
+  , turnsCompleted
   , firstTurn
   ) where
 
 newtype Turn = Turn Int
   deriving (Eq, Show)
+
+turnsCompleted :: Turn -> Int
+turnsCompleted (Turn x) = x - 1
 
 firstTurn :: Turn
 firstTurn = Turn 1
