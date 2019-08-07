438bc226b36c39a3f6c0f2d5dc74462699db5c52
Unknown <nick.a.lawton@gmail.com>
2019-08-07 23:55:25 +1000

    Higher value victory cards should have larger affect on score

--- test/ScoringTests.hs
+++ test/ScoringTests.hs
@@ -3,6 +3,7 @@ module ScoringTests (scoringTests) where
 import Scoring
 import Player
 import CompletePlayer
+import Card
 
 import ArbitraryInstances
 import Test.Hspec
@@ -12,7 +13,14 @@ scoringTests :: SpecWith ()
 scoringTests = describe "scoring" $ do
 
   it "increases after adding a victory card" $ property $ \player (VictoryCard card) ->
-    score player <= score (alterDeck (card :) (player :: CompletePlayer))
+    score (player :: CompletePlayer) <= score (alterDeck (card :) player)
 
   it "does not change after adding a non-victory card" $ property $ \player (NonVictoryCard card) ->
-    score player == score (alterDeck (card :) (player :: CompletePlayer))
+    score (player :: CompletePlayer) == score (alterDeck (card :) player)
+
+  it "increases relative to cost of victory card" $ property $ \player ->
+    addAndScore Province player > addAndScore Duchy player
+      && addAndScore Duchy player > addAndScore Estate player
+        where
+          addAndScore :: Card -> CompletePlayer -> Int
+          addAndScore card p = score (alterDeck (card :) p)
